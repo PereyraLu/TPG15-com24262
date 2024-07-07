da9dd7f7c6693ed071bfea405913c52753e93395
Leonardo Cortes <leosirx@gmail.com>
2024-07-07 17:52:14 -0300

    deploy-bd

--- app.py
+++ app.py
@@ -1,99 +1,89 @@
-from flask import Flask, render_template,request,redirect,url_for,flash
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import os
-app=Flask(__name__)
+
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/img/products'
 
-#MySQL connection
-app.config['MYSQL_HOST']='dpg-cq5dvalds78s73d0i390-a.oregon-postgres.render.com'
-app.config['MYSQL_USER']='pereyralu'
-app.config['MYSQL_PASSWORD']='eR3SjO1US6tNoCqOwv3PNPWFxgO2z4qX'
-app.config['MYSQL_DB']='products_40gm'
-mysql=MySQL(app)
+# PostgreSQL connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iopleylx:FqBZHn28tHT2zLzdxqtE25JCcGXewZXf@motty.db.elephantsql.com/iopleylx'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-#settings
-app.secret_key='mysecretkey'
+# Settings
+app.secret_key = 'mysecretkey'
+
+# Model definition
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    photo = db.Column(db.Text)
+    amount = db.Column(db.Integer, nullable=False)
 
 @app.route("/")
 def index():
-    cur= mysql.connection.cursor()
-    cur.execute('SELECT * FROM products')
-    data=cur.fetchall()
-    return render_template("index.html",products=data)
+    products = Product.query.all()
+    return render_template("index.html", products=products)
 
-@app.route('/register_product', methods=['GET','POST'])
+@app.route('/register_product', methods=['GET', 'POST'])
 def addProduct():
     return render_template("register.html")
 
-@app.route('/add_product',methods=['POST'])
+@app.route('/add_product', methods=['POST'])
 def formProduct():
-    if request.method== 'POST':
-        category=request.form['category']
-        name=request.form['name']
-        price=request.form['price']
-        amount=request.form['amount']
-        photo=request.files['photo']
+    if request.method == 'POST':
+        category = request.form['category']
+        name = request.form['name']
+        price = request.form['price']
+        amount = request.form['amount']
         if 'photo' in request.files:
-            photo_file=request.files['photo']
+            photo_file = request.files['photo']
             if photo_file.filename != '':
                 filename = secure_filename(photo_file.filename)
-        photo_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        cur=mysql.connection.cursor()
-        cur.execute('INSERT INTO products (category,name,price,amount,photo) VALUES (%s,%s,%s,%s,%s)',(category,name,price,amount,filename))
-        mysql.connection.commit()
-        flash("Contact added successfully")
+                photo_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                new_product = Product(category=category, name=name, price=price, amount=amount, photo=filename)
+                db.session.add(new_product)
+                db.session.commit()
+                flash("Product added successfully")
         return redirect(url_for('index'))
 
-
-@app.route("/edit/<id>")
+@app.route("/edit/<int:id>")
 def getProduct(id):
-    cur=mysql.connection.cursor()
-    cur.execute('SELECT * FROM products WHERE id=%s',(id,))
-    data=cur.fetchall()
-    return render_template('edit.html',product=data[0])
+    product = Product.query.get_or_404(id)
+    return render_template('edit.html', product=product)
 
-@app.route("/update/<id>",methods=['POST'])
+@app.route("/update/<int:id>", methods=['POST'])
 def updateProduct(id):
-    if request.method== 'POST':
-        category=request.form['category']
-        name=request.form['name']
-        price=request.form['price']
-        amount=request.form['amount']
-        photo=request.files['photo']
+    product = Product.query.get_or_404(id)
+    if request.method == 'POST':
+        product.category = request.form['category']
+        product.name = request.form['name']
+        product.price = request.form['price']
+        product.amount = request.form['amount']
         if 'photo' in request.files:
-            photo_file=request.files['photo']
+            photo_file = request.files['photo']
             if photo_file.filename != '':
                 filename = secure_filename(photo_file.filename)
-        photo_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        cur=mysql.connection.cursor()
-        cur.execute("""
-                UPDATE products
-                SET category=%s,
-                    name=%s,
-                    price=%s,
-                    amount=%s,
-                    photo=%s
-                WHERE id=%s
-                """,(category,name,price,amount,filename,id))
-        mysql.connection.commit()
-        flash("Contact added successfully")
+                photo_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                product.photo = filename
+        db.session.commit()
+        flash("Product updated successfully")
         return redirect(url_for('index'))
-    
-    
-@app.route("/delete/<string:id>")
+
+@app.route("/delete/<int:id>")
 def deleteProduct(id):
-    cur=mysql.connection.cursor()
-    cur.execute('DELETE FROM products WHERE id={0}'.format(id))
-    mysql.connection.commit()
-    flash("Contact deleted successfully")
+    product = Product.query.get_or_404(id)
+    db.session.delete(product)
+    db.session.commit()
+    flash("Product deleted successfully")
     return redirect(url_for('index'))
 
 @app.errorhandler(404)
 def not_found(error):
-        return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
-
-""" Prueba de cambios """
